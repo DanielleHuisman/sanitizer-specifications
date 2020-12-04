12a9124314a78e060efb6b622a090b3ae1173a57
Daniel Huisman <daniel@huisman.me>
2020-12-04 20:30:57 +0100

    Add generator for basic SFA range list specifications

---
+++ src/main/java/io/danielhuisman/sanitizers/generators/sfa/GeneratorRangeList.java
@@ -0,0 +1,70 @@
+package io.danielhuisman.sanitizers.generators.sfa;
+
+import automata.sfa.SFAInputMove;
+import automata.sfa.SFAMove;
+import io.danielhuisman.sanitizers.sfa.SFAWrapper;
+import org.apache.commons.lang3.tuple.Pair;
+import org.sat4j.specs.TimeoutException;
+import theory.characters.CharPred;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GeneratorRangeList extends SFAGenerator<Collection<Pair<CharPred, Integer>>> {
+    @Override
+    public String getName() {
+        return "range-list";
+    }
+
+    @Override
+    public SFAWrapper generate(Collection<Pair<CharPred, Integer>> input) throws TimeoutException {
+        Collection<SFAMove<CharPred, Character>> transitions = new LinkedList<>();
+        Collection<Integer> finalStates = new HashSet<>();
+
+        // Add transitions for each range
+        int index = 0;
+        for (Pair<CharPred, Integer> inputRange : input) {
+            // Add transitions for the specified amount of this range
+            for (int i = 0; i < inputRange.getRight(); i++) {
+                transitions.add(new SFAInputMove<>(index, index + 1, inputRange.getLeft()));
+                index++;
+            }
+        }
+
+        // Mark last state as final
+        finalStates.add(index);
+
+        return new SFAWrapper(transitions, 0, finalStates, false);
+    }
+
+    @Override
+    public Collection<Pair<String, SFAWrapper>> generateExamples() throws TimeoutException {
+        List<Pair<String, SFAWrapper>> examples = new LinkedList<>();
+
+        examples.add(Pair.of("range1", generate(
+                List.of(
+                        Pair.of(new CharPred('A', 'Z'), 3),
+                        Pair.of(new CharPred('-'), 1),
+                        Pair.of(new CharPred('0', '9'), 1)
+                )
+        )));
+
+        return examples;
+    }
+
+    @Override
+    public String format(Collection<Pair<CharPred, Integer>> input) {
+        return String.format("[%s]", input
+                .stream()
+                .map(inputRange -> String.format("%s x%d", inputRange.getLeft().toString(), inputRange.getRight()))
+                .collect(Collectors.joining(", ")));
+    }
+
+    @Override
+    public Collection<Pair<CharPred, Integer>> parse(String input) {
+        return null;
+    }
+}
---
+++ src/test/java/io/danielhuisman/sanitizers/generators/TestGeneratorRangeList.java
@@ -0,0 +1,74 @@
+package io.danielhuisman.sanitizers.generators;
+
+import io.danielhuisman.sanitizers.generators.sfa.GeneratorRangeList;
+import io.danielhuisman.sanitizers.sfa.SFAWrapper;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.Test;
+import org.sat4j.specs.TimeoutException;
+import theory.characters.CharPred;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestGeneratorRangeList {
+
+    private final GeneratorRangeList generator = new GeneratorRangeList();
+
+    @Test
+    public void test() throws TimeoutException {
+        test(List.of(
+                Pair.of(new CharPred('A', 'Z'), 3)
+        ), List.of("ABC", "ZYX"), List.of("abc", "a", "", "AbC", "343"));
+
+        test(List.of(
+                Pair.of(new CharPred('A', 'Z'), 3),
+                Pair.of(new CharPred('-'), 1),
+                Pair.of(new CharPred('0', '9'), 1)
+        ), List.of("ABC-5", "ZYX-3"), List.of("abc", "a", "", "AbC9", "343", "abc-1", "QCE--1"));
+    }
+
+    private void test(Collection<Pair<CharPred, Integer>> input, Collection<String> accept, Collection<String> reject)
+            throws TimeoutException {
+        SFAWrapper sfa = generator.generate(input);
+
+        for (String word : accept) {
+            assertTrue(sfa.accepts(word), String.format("%s should accept string \"%s\"", generator.format(input), word));
+        }
+
+        for (String word : reject) {
+            assertFalse(sfa.accepts(word), String.format("%s should not accept \"%s\"", generator.format(input), word));
+        }
+    }
+
+    @Test
+    public void testFormat() {
+        assertEquals("[[A-Z] x10]", generator.format(
+                List.of(
+                        Pair.of(new CharPred('A', 'Z'), 10)
+                )
+        ));
+
+        assertEquals("[[A-Z] x3, [\\--\\\\] x1, [0-9] x2]", generator.format(
+                List.of(
+                        Pair.of(new CharPred('A', 'Z'), 3),
+                        Pair.of(new CharPred('-', '\\'), 1),
+                        Pair.of(new CharPred('0', '9'), 2)
+                )
+        ));
+    }
+
+//    @Test
+//    public void testParse() {
+//        assertEquals(List.of(
+//                Pair.of(new CharPred('A', 'Z'), 10)
+//        ), generator.parse("[[A-Z] x10]"));
+//
+//        assertEquals(List.of(
+//                Pair.of(new CharPred('A', 'Z'), 3),
+//                Pair.of(new CharPred('-', '\\'), 1),
+//                Pair.of(new CharPred('0', '9'), 2)
+//        ), generator.parse("[[A-Z] x3, [\\--\\\\] x1, [0-9] x2]"));
+//    }
+}
